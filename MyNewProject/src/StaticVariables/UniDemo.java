394df4efaa917a5f7d3d2767f9b784daaee1bd44
Esmadula Hasani <assmat2004@hotmail.com>
2024-07-08 18:15:41 +0100

    new files

---
+++ MyNewProject/src/StaticVariables/UniDemo.java
@@ -0,0 +1,12 @@
+package StaticVariables;
+
+public class UniDemo {
+
+	public static void main(String[] args) {
+		// Here i call the static method by class name which is University.
+		
+		University.displayUniversityInfo();
+
+	}
+
+}
